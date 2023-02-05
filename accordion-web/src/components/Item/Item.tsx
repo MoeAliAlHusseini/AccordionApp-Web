8951eaa165f844a8c3dd96f084c48b3bbe3b5a5b
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2023-02-05 18:23:23 +0400

    Create the Item component

---
+++ accordion-web/src/components/Item/Item.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Item.css'
+
+const Item = () => {
+    return (
+        <div className='container'>
+            <p>
+                Item!
+            </p>
+        </div>
+    )
+}
+
+export default Item;
