767fff80b79aac279a6a278485e64a0c63792d91
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2023-02-12 13:07:52 +0400

    Create the List component

---
+++ accordion-web/src/components/List/List.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const List = () => {
+    return (
+        <div>
+            <p>
+                Hello
+            </p>
+        </div>
+    )
+}
+
+export default List;
