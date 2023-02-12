48044782507e75c47eb0d7b1f8dbf1468d00cabe
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2023-02-12 16:22:17 +0400

    Refactor the item component along with its styling

--- accordion-web/src/components/Item/Item.tsx
+++ accordion-web/src/components/Item/Item.tsx
@@ -5,12 +5,21 @@ import './Item.css'
 interface ItemProps {
     name: string,
     occupation: string,
-    residence: string
+    residence: string,
+    style?: Object,
+    className?: string
+}
+
+const style = {
+    item: {
+        backgroundColor: 'white',
+        borderWidth: 3
+    }
 }
 
 const Item = ({ name, occupation, residence }: ItemProps) => {
     return (
-        <div className='container'>
+        <div className='container' style={style.item}>
             <p>
                 {`Name: ${name}`}
             </p>
