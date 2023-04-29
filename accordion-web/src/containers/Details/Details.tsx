228a7ead7b1fd8399fec80a8184d2f89c4620602
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2023-04-29 20:44:36 +0400

    Implement sass styling for Details and add new logic

--- accordion-web/src/containers/Details/Details.tsx
+++ accordion-web/src/containers/Details/Details.tsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+import './Details.scss'
+
 const Details = () => {
+    const [inputValue, setInputValue] = useState('')
+    const [titleValue, setTitleValue] = useState('')
+
     const location = useLocation();
 
+    useEffect(() => {
+        setTitleValue(location.state.id);
+        setInputValue(location.state.id);
+    }, [location.state.id])
+
+    const handleButtonOnChange = (e: any) => {
+        const target = e.target;
+        const value = target.value;
+
+        setInputValue(value);
+    }
+
+    const handleTitleValue = () => {
+        setTitleValue(inputValue)
+    }
+
+
     return (
-        <div>
+        <div className="Details">
             <h1>
-                {`Hello ${location.state.id}`}
+                {`Hello ${titleValue}`}
             </h1>
+            <input
+                className="Details__input"
+                value={inputValue}
+                onChange={handleButtonOnChange}
+            />
+            <button className="Details__button"
+                title="value"
+                onClick={handleTitleValue}
+            >
+                <text>
+                    {'Press me!'}
+                </text>
+            </button>
+
         </div>
     )
 }
